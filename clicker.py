653a2d7614e62ff8999bbdd08fcea7f8d60d22d8
mmann <m4xmann@gmail.com>
2025-05-11 16:56:27 +0300

    Moved Bus to a Class

--- clicker.py
+++ clicker.py
@@ -3,31 +3,68 @@ import os
 import random
 import math
 
+pg.init()#Start Pygame
+bg = pg.display.set_mode((500, 400))#Open a 500x400 pixel window
+
+class GameState:
+    def __init__(self):
+        self.Clicks = 0
+        self.Pressed = 0;
+    def addClick(self,n):
+        self.Clicks = self.Clicks + n
+
+class Bus:
+    damage0Image = pg.image.load('graphics/blue bus.png').convert_alpha()
+    bumpSound=pg.mixer.Sound("BlueBusBump.mp3")
+    bumpSound.set_volume(.1)
+    def __init__(self,startingYPos,gs):
+        self.startingYPos=startingYPos;
+        self.YPos=startingYPos;
+        self.YVel=0;
+        self.XPos=-self.damage0Image.get_rect()[2]
+        self.XVel=3;
+        self.gs = gs;
+    def simulate(self):#Advance the bus to its next simulated physics state
+        mouse_x, mouse_y = pg.mouse.get_pos()
+        if self.damage0Image.get_rect().collidepoint(mouse_x-self.XPos, mouse_y-self.YPos) and pg.mouse.get_pressed()[0] and self.gs.Clicks > 0 and not self.gs.Pressed:#Clicking on the bus increases its X Velocity
+            self.XVel = self.XVel + 1
+            self.gs.addClick(-1)
+            #TO DO: Make a sound of coins going into the payment machine
+        self.XPos = self.XPos + self.XVel
+        if self.XPos > 550:
+            self.XPos=-self.damage0Image.get_rect()[2] # Wrap bus
+        self.YVel=self.YVel+.75 #Gravitational Acceleration
+        if random.random()<0.01*self.XVel and self.YPos==self.startingYPos:#Bus is driving on its ground level and hits a random bump
+            self.YVel=self.YVel-5
+            self.gs.addClick(1)
+            self.bumpSound.play()
+        if random.random()<0.01 and self.YPos==self.startingYPos and self.XVel > 0:#Bus breaks down
+            self.XVel = self.XVel - 1
+            #TO DO: If we break down to speed zero, play a sound of the driver saying "Not again!"
+        self.YPos=self.YPos+self.YVel
+        if self.YPos>self.startingYPos:#Bus lands back on its ground level
+            self.YPos=self.startingYPos
+            self.YVel=0
+        bg.blit(pg.transform.rotate(self.damage0Image,-self.YVel),(self.XPos,self.YPos-math.sin(abs(self.YVel)*math.pi/180)*self.damage0Image.get_rect()[2]/2))
+    #TO DO: User can click on the bus to speed it up
 
 def main():#Create the main function
-
-    pg.init()#Start Pygame
-    bg = pg.display.set_mode((500, 400))#Open a 500x400 pixel window
 
     SpaceBar=pg.image.load('graphics/Button1.png').convert_alpha()
     SpaceBarGoingDown=pg.image.load('graphics/Button2.png').convert_alpha()
     SpaceBarDown=pg.image.load('graphics/Button3.png').convert_alpha()
-    BlueBusUpgrade = pg.image.load('graphics/blue bus.png').convert_alpha()
     SpaceBar=pg.transform.scale_by(SpaceBar,1)
     pg.mixer.init()
     Beep=pg.mixer.Sound("Easytotype.mp3")
-    BlueBusBump=pg.mixer.Sound("BlueBusBump.mp3")
-    BlueBusBump.set_volume(.1)
     bg.fill((127, 127, 127))#fill the background gray
     clock = pg.time.Clock()#Get a copy of the clock object (slow?)
     pg.font.init()#Set up all fonts for use (probably not needed)
     font1 = pg.font.Font(None, 100)#Get a copy of the default font
-    Clicks = 0;
-    BlueBusXPos=-BlueBusUpgrade.get_rect()[2]
+
     #Initialize Pressed
-    Pressed=0;
-    BlueBusYPos=200;
-    BlueBusYVel=0;
+    gs = GameState();
+    #For testing:
+    buses = [Bus(200+30*i,gs) for i in range(8)];
 
     while True:#Main game loop (continues forever)
         clock.tick(50)#Run at 50 Frames per second
@@ -37,7 +74,7 @@ def main():#Create the main function
                 raise SystemExit#Error out the program
             if event.type==pg.KEYDOWN:
                 if event.key==pg.K_SPACE:
-                    Clicks=Clicks+1
+                    gs.addClick(1)
                     Beep.play()
         #Erase the screen
         bg.fill((127, 127, 127))#fill the background gray
@@ -50,32 +87,21 @@ def main():#Create the main function
         mouse_x, mouse_y = pg.mouse.get_pos()
 
         SpaceBarXPos=bg.get_rect()[2]/2-SpaceBar.get_rect()[2]/2
-        if BlueBusXPos<550:
-            BlueBusXPos=BlueBusXPos+1
-        else:
-            BlueBusXPos=-BlueBusUpgrade.get_rect()[2] # Wrap bus
         if SpaceBar.get_rect().collidepoint(mouse_x-0, mouse_y-200) and pg.mouse.get_pressed()[0] or pg.key.get_pressed()[pg.K_SPACE]:
             bg.blit(SpaceBarGoingDown,(SpaceBarXPos,200))
         else:
             bg.blit(SpaceBar,(SpaceBarXPos,200))
-        BlueBusYVel=BlueBusYVel+.75 #Gravitational Acceleration
-        if random.random()<0.01 and BlueBusYPos==200:
-            BlueBusYVel=BlueBusYVel-5
-            BlueBusBump.play()
-        BlueBusYPos=BlueBusYPos+BlueBusYVel
-        if BlueBusYPos>200:
-            BlueBusYPos=200
-            BlueBusYVel=0
-        bg.blit(pg.transform.rotate(BlueBusUpgrade,-BlueBusYVel),(BlueBusXPos,BlueBusYPos-math.sin(abs(BlueBusYVel)*math.pi/180)*BlueBusUpgrade.get_rect()[2]/2))
-      
-        if SpaceBar.get_rect().collidepoint(mouse_x-0, mouse_y-200) and pg.mouse.get_pressed()[0] and not Pressed:
-            Clicks=Clicks+1
+        
+        if SpaceBar.get_rect().collidepoint(mouse_x-0, mouse_y-200) and pg.mouse.get_pressed()[0] and not gs.Pressed:
+            gs.addClick(1)
             Beep.play()
-        Pressed=pg.mouse.get_pressed()[0]
         #Update the text
-        button_text = font1.render("Clicks:%d"%Clicks, False, (0, 0, 0))#Like printing this text onto a slip of paper...
+        button_text = font1.render("Clicks:%d"%gs.Clicks, False, (0, 0, 0))#Like printing this text onto a slip of paper...
         bg.blit(button_text,[0,0,200,100]);#...which we glue onto the screen at the rectangle coordinates 0,0, which is the upper left corner, because of computer graphics conventions.
-    
+        #Update the bus
+        [bus.simulate() for bus in buses];
+        
+        gs.Pressed=pg.mouse.get_pressed()[0]
         pg.display.update()#Draw the display
 
 
